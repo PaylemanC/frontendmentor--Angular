553de42429c867087e6020825dbb834e6b6f6ed2
Carina Payleman <rociocarina2@gmail.com>
2023-09-04 21:23:22 -0300

    Create modules newbie (difficulty) and shared

---
+++ src/app/newbie/newbie.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewbieRoutingModule } from './newbie-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NewbieRoutingModule
+  ]
+})
+export class NewbieModule { }
