80fa6d6f4313daa7f8373844f9d7a14ef3d395d6
Carina Payleman <rociocarina2@gmail.com>
2023-09-05 19:09:42 -0300

    Insert footer & module-card as first screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<main><app-module-card></app-module-card></main><app-footer></app-footer>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
