0cade8d2802af94b7139ce79f949cfe02949b62f
Carina Payleman <rociocarina2@gmail.com>
2023-09-06 19:24:44 -0300

    Start new challenge: Product preview card, HTML & CSS

---
+++ src/app/newbie/product-preview-card/product-preview-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-preview-card',
+  templateUrl: './product-preview-card.component.html',
+  styleUrls: ['./product-preview-card.component.scss']
+})
+export class ProductPreviewCardComponent {
+
+}
