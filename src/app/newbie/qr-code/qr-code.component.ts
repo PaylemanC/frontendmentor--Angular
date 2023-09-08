8068217c9e5bd2aa93932702542fb3d08ad26cb8
Carina Payleman <rociocarina2@gmail.com>
2023-09-07 22:28:47 -0300

    Create challenge data model, preview in qr-code challenge

--- src/app/newbie/qr-code/qr-code.component.ts
+++ src/app/newbie/qr-code/qr-code.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Challenge, Difficulty, Language } from 'src/app/shared/models/challenge.model';
 
 @Component({
   selector: 'app-qr-code',
   templateUrl: './qr-code.component.html',
   styleUrls: ['./qr-code.component.scss']
 })
-export class QrCodeComponent {
-
+export class QrCodeComponent implements Challenge {
+  id = 'iux_sIO_H';
+  name = 'QR Code';
+  img = './src/assets/previews/newbie/qr-code-preview.jpg';
+  description = 'Build the Frontend QR Code card.';
+  languages = [Language.HTML, Language.CSS];
+  difficulty = Difficulty.NEWBIE;
 }
---
+++ src/app/shared/models/challenge.model.ts
@@ -0,0 +1,23 @@
+export enum Difficulty {
+  NEWBIE = 'NEWBIE',
+  JUNIOR = 'JUNIOR',
+  INTERMEDIATE = 'INTERMEDIATE',
+  ADVANCED = 'ADVANCED',
+  GURU = 'GURU',
+}
+
+export enum Language {
+  HTML = 'HTML',
+  CSS = 'CSS',
+  API = 'API',
+  TS = 'TS'
+}
+
+export interface Challenge {
+  id: string | number;
+  name: string;
+  img: string; //preview
+  description: string;
+  languages: Language[];
+  difficulty: Difficulty;
+}
