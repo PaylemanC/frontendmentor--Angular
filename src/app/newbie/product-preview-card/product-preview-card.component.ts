f36e9313063ea6d788446f0bf7d07d9551c02d9c
Carina Payleman <rociocarina2@gmail.com>
2023-09-08 17:14:15 -0300

    Add challenge data

--- src/app/newbie/product-preview-card/product-preview-card.component.ts
+++ src/app/newbie/product-preview-card/product-preview-card.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Challenge, Difficulty, Language } from 'src/app/shared/models/challenge.model';
 
 @Component({
   selector: 'app-product-preview-card',
   templateUrl: './product-preview-card.component.html',
   styleUrls: ['./product-preview-card.component.scss']
 })
-export class ProductPreviewCardComponent {
-
+export class ProductPreviewCardComponent implements Challenge {
+  id = 'GO7UmttRfa';
+  name = 'Product Preview Card';
+  img = './src/assets/previews/newbie/product-preview-card.jpg';
+  description = 'Build a responsive product (perfume) preview card.';
+  languages = [Language.HTML, Language.CSS];
+  difficulty = Difficulty.NEWBIE;
+  url = 'https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa';
 }
