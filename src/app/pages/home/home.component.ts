7f4105f1b9a7e18a468a6837914f81c60ba01ae0
Carina Payleman <rociocarina2@gmail.com>
2023-09-08 20:35:52 -0300

    Modify Home: show all projects as MVP

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<main><app-module-card></app-module-card></main>`,
+  template: `<main><app-project-card></app-project-card></main>`,
+  //<main><app-module-card></app-module-card></main>
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
