35d5feed7c3e32b2d746b2252df14ef7dabc4696
Carina Payleman <rociocarina2@gmail.com>
2023-09-13 21:19:18 -0300

    Add quick link to results-summary.component

--- src/app/newbie/newbie-routing.module.ts
+++ src/app/newbie/newbie-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { QrCodeComponent } from './qr-code/qr-code.component';
 import { ProductPreviewCardComponent } from './product-preview-card/product-preview-card.component';
-import { InteractiveRatingComponent } from './interactive-rating/interactive-rating.component'
+import { InteractiveRatingComponent } from './interactive-rating/interactive-rating.component';
+import { ResultsSummaryComponent } from './results-summary/results-summary.component';
 
 const routes: Routes = [
   { path: 'qr-code', component: QrCodeComponent },
   { path: 'product-preview-card', component: ProductPreviewCardComponent },
-  { path: 'interactive-rating', component: InteractiveRatingComponent}
+  { path: 'interactive-rating', component: InteractiveRatingComponent},
+  { path: 'results-summary', component: ResultsSummaryComponent}
 ];
 
 @NgModule({
--- src/app/newbie/newbie.module.ts
+++ src/app/newbie/newbie.module.ts
@@ -5,12 +5,14 @@ import { NewbieRoutingModule } from './newbie-routing.module';
 import { QrCodeComponent } from './qr-code/qr-code.component';
 import { ProductPreviewCardComponent } from './product-preview-card/product-preview-card.component';
 import { InteractiveRatingComponent } from './interactive-rating/interactive-rating.component';
+import { ResultsSummaryComponent } from './results-summary/results-summary.component';
 
 @NgModule({
   declarations: [
     QrCodeComponent,
     ProductPreviewCardComponent,
-    InteractiveRatingComponent
+    InteractiveRatingComponent,
+    ResultsSummaryComponent
   ],
   imports: [
     CommonModule,
